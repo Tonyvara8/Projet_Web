6af7027688f50f474137947440ed6eb2d9be6b4a
Tonyvara8 <151458140+Tonyvara8@users.noreply.github.com>
2025-05-07 19:21:02 +0200

    ajout vidéo introduction

---
+++ Projet/javascript/script.js
@@ -0,0 +1,25 @@
+const video = document.getElementById('introvid');
+const intro = document.getElementById('intro');
+const main = document.getElementById('main');
+
+if (localStorage.getItem('video') === 'vu') {
+    intro.style.display = 'none';
+    document.body.style.overflow = 'auto';
+
+    // Empêche la vidéo de jouer dans le fond
+    video.pause();
+    video.currentTime = 0;
+} else {
+    video.onended = () => {
+        // Lancer l’animation
+        video.style.animation = 'shrinkAndSlide 2s forwards';
+
+        // Attendre la fin de l'animation
+        video.addEventListener('animationend', () => {
+            intro.style.display = 'none';
+            main.style.display = 'flex';
+            document.body.style.overflow = 'auto';
+            localStorage.setItem('video', 'vu');
+        });
+    };
+}
