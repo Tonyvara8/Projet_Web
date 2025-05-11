b44d2b39115589d4edc811c978fe9dd6f108f07e
Tonyvara8 <151458140+Tonyvara8@users.noreply.github.com>
2025-05-11 16:40:18 +0200

    ajout page chargement et sélection de ville

--- Projet/javascript/scriptmap.js
+++ Projet/javascript/scriptmap.js
@@ -4,6 +4,46 @@
       attribution: '© OpenStreetMap contributors'
     }).addTo(map);
 
+
+ button= document.getElementById("searchbtn"); 
+button.addEventListener('click',(event) =>{
+const load =document.getElementById("chargement");
+ chargement.style.display="block";
+var ville = document.getElementById("bar");
+var villeStock = ville.value; 
+console.log(ville.value);
+ville.value="";
+console.log(villeStock);
+var query = `[out:json][timeout:25];
+nwr["name"~"^${villeStock}", i];
+out center;`
+  fetch("https://overpass-api.de/api/interpreter", {
+    method: "POST",
+    
+    body: query
+  })
+    .then(response => {
+      if (!response.ok) throw new Error("Erreur serveur Overpass");
+      return response.json();
+     
+
+    })
+    .then(data => {
+      console.log("Résultat Overpass :", data);
+      if (data.elements.length > 0) {
+        const coord = data.elements[0];
+       var lat = coord.lat;
+      var long = coord.lon;
+      map.setView([lat, long], 11); // Centrer la carte sur la ville
+      } else {
+        console.log("Aucune ville trouvée.");
+      }
+    })
+    .catch(error => {
+      console.error("Erreur fetch Overpass :", error);
+          })
+     
+ map.once('moveend', () => {
     const bounds = map.getBounds();    // Donne les limites de la map
     const recherche = `
       [out:json][timeout:25];
@@ -25,9 +65,9 @@
       const nomsalle =element.tags.name || "salle de sport"; // Récupère le nom de la salle ou si y'a pas => salle de sport
        if(lat&&long){
         L.marker([lat, long]).addTo(map).bindPopup(nomsalle);   // affiche un popup au marqueur de la salle avec son nom
-         } ;}
-        );
-       })
-    .catch(error => {
-      console.error("Erreur avec Overpass API:", error);
-    });
+       } ;}
+      );
+      chargement.style.display="none";})
+      .catch(error => {
+        console.error("Erreur avec Overpass API:", error);
+      })})});
