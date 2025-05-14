7f2f19333d2f5ac0772b931aba5500f703ecdaa5
Tonyvara8 <151458140+Tonyvara8@users.noreply.github.com>
2025-05-14 13:36:52 +0200

    .

--- Projet/javascript/script.js
+++ Projet/javascript/script.js
@@ -1,31 +1,6 @@
-const video = document.getElementById('introvid');
-const intro = document.getElementById('intro');
-const main = document.getElementById('main');
-
-if (localStorage.getItem('video') === 'vu') {
-    intro.style.display = 'none';
-    document.body.style.overflow = 'auto';
-
-    // Empêche la vidéo de jouer dans le fond
-    video.pause();
-    video.currentTime = 0;
-} else {
-    video.onended = () => {
-        // Lancer l’animation
-        video.style.animation = 'shrinkAndSlide 2s forwards';
-
-        // Attendre la fin de l'animation
-        video.addEventListener('animationend', () => {
-            intro.style.display = 'none';
-            main.style.display = 'flex';
-            document.body.style.overflow = 'auto';
-            localStorage.setItem('video', 'vu');
-        });
-    };
-}
-
-window.addEventListener('beforeunload', () => {
-    // Réinitialiser la vidéo
-    video.pause();
-    video.currentTime = 0;
+const menu = document.getElementById('menu');
+const link = document.getElementById('list');
+menu.addEventListener('click', function() {
+	link.style.display = 'block';
+	console.log('ok');
 });
