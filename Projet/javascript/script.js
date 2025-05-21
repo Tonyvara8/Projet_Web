f974190acf78c58993b765f8bfd37e44f0779da2
Tonyvara8 <151458140+Tonyvara8@users.noreply.github.com>
2025-05-21 11:47:38 +0200

    W3C check

--- Projet/javascript/script.js
+++ Projet/javascript/script.js
@@ -1,9 +1,15 @@
 const menu = document.getElementById('menu');
-const link = document.getElementById('list');
+const link = document.getElementsByClassName('nav-link')[0]
 menu.addEventListener('click', function() {
-    if (link.style.display === 'flex') {
-        link.style.display = 'none';
-    }
+const open = document.getElementById('open');
+if (open === null) {
+    open= document.createElement('div');
+    open.setAttribute('id', 'open');
+        link.appendChild(open);
+    console.log('caa');
+
+
+}
     else{
 	link.style.display = 'flex';}
 	console.log('ok');
