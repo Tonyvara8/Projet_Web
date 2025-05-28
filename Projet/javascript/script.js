2642695f52d67f9331d9d9da1e7213a4cbf70fd6
Tonyvara8 <151458140+Tonyvara8@users.noreply.github.com>
2025-05-28 09:06:08 +0200

    modification page d'accueil

--- Projet/javascript/script.js
+++ Projet/javascript/script.js
@@ -1,15 +1,16 @@
-const menu = document.getElementById('menu');
-const link = document.getElementsByClassName('nav-link')[0];
-menu.addEventListener('click', function() {
-    let open = document.getElementById('open');
-    if (open === null) {
-        open = document.createElement('div');
-        open.setAttribute('id', 'open');
-        link.appendChild(open);
-        console.log('caa');
-    }
-    else {
-        link.style.display = 'flex';
-    }
-    console.log('ok');
+const titre = document.getElementById("content");
+window.addEventListener("load", () => {
+setTimeout(() => {
+    titre.style.top ="10%"; 
+
+
+},1000)
+
+setTimeout(() => {
+    const citation= document.createElement("h2");
+citation.textContent="Devenez la meilleure version de vous";
+citation.setAttribute("id", "citation");
+
+document.getElementById("accueil").appendChild(citation);}
+, 2000)
 });
